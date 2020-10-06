072ec62af81b6fdf4088e7e28954914bef6f56a0
millertron <senjinkoha@gmail.com>
2020-10-06 18:06:13 +0100

    Display placeholder calendar pockets

--- vue-app/src/components/Calendar.vue
+++ vue-app/src/components/Calendar.vue
@@ -1,14 +1,35 @@
 <template>
     <div class='calendar'>
         <div class='d-flex justify-content-center flex-wrap'>
-        
+            <CalendarPocket v-for='pocket in calendarPockets' v-bind:key='pocket.id'
+                v-bind:dayNum='pocket.dayNum' />        
         </div>
         <div class='mt-5'>
             <span class='pt-5 small'>
                 Powered by Millertronic Systems™. <br/>
-                Pokémon images &amp; names © Nintendo/Game Freak 1995-%%currentYear%%. <br/>
-                Pokémon sprite links © Pokémon Database, 2008-%%currentYear%%.
+                Pokémon images &amp; names © Nintendo/Game Freak 1995-{{currentYear}}. <br/>
+                Pokémon sprite links © Pokémon Database, 2008-{{currentYear}}.
             </span>
         </div>
     </div>
 </template>
+
+<script>
+import CalendarPocket from './CalendarPocket'
+
+export default {
+    components: {
+        CalendarPocket
+    },
+
+    data: () => {
+        return {
+            calendarPockets: [ {id: 1, dayNum: 1}, {id: 2, dayNum: 2}, {id: 3, dayNum: 3} ]
+        }
+    },
+    
+    computed: {
+        currentYear: () => new Date().getFullYear()
+    }
+}
+</script>
--- vue-app/src/components/CalendarPocket.vue
+++ vue-app/src/components/CalendarPocket.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <div class='card pocket-card'>
-
+            pocket {{dayNum}}
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        dayNum: Number
+    }
+}
+</script>
