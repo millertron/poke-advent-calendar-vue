9eaf1773721791e28ee522e3a34dc2de804eea35
millertron <senjinkoha@gmail.com>
2020-10-07 21:18:26 +0100

    Enable CORS

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./helpers/database')
 const pocketRoutes = require('./routes/pockets')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3200
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3200
 app.get('/', (req, res) => res.send("For the advent calendar app, please navigate to &#39;/adventcalendar/&#39;" 
     + " followed by the URL key provided by the administrator."))
 
+app.use(cors())
 app.use(express.json())
 app.use('/pockets', pocketRoutes)
 
