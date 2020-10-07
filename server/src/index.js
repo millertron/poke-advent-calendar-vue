5f8ef9643fd5244c8e56f93465408bf4262de313
millertron <senjinkoha@gmail.com>
2020-10-07 21:07:01 +0100

    Connect server to database and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const db = require('./helpers/database')
+const pocketRoutes = require('./routes/pockets')
 
 const app = express()
 const port = process.env.PORT || 3200
@@ -7,5 +9,8 @@ app.get('/', (req, res) => res.send("For the advent calendar app, please navigat
     + " followed by the URL key provided by the administrator."))
 
 app.use(express.json())
+app.use('/pockets', pocketRoutes)
 
-app.listen(port, ()=>(console.log(`Poke Express Running on Port ${port}!`)));
+db.connect((err, client) => {
+    app.listen(port, ()=>(console.log(`Poke Express Running on Port ${port}!`)));
+})
