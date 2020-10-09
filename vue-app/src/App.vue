7e65108c203256e68218148153c10fd419d4be3e
millertron <senjinkoha@gmail.com>
2020-10-09 08:45:25 +0100

    Additional styling

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <router-view />
-    <div class='mt-5'>
-        <span class='pt-5 small'>
-            Powered by Millertronic Systems™. <br/>
-            Pokémon images &amp; names © Nintendo/Game Freak 1995-{{currentYear}}. <br/>
-            Pokémon sprite links © Pokémon Database, 2008-{{currentYear}}.
-        </span>
-    </div>
+    <footer class='page-footer'>
+      <div class='mt-5'>
+          <span class='pt-5 small'>
+              Powered by Millertronic Systems™. <br/>
+              Pokémon images &amp; names © Nintendo/Game Freak 1995-{{currentYear}}. <br/>
+              Pokémon sprite links © Pokémon Database, 2008-{{currentYear}}.
+          </span>
+      </div>
+    </footer>
   </div>
 </template>
 
